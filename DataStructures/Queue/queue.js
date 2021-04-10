d790163f230affe6c8afcadad1ae3dbef3be8c67
Jitendra Sabat <jitendrasabat.jitu@gmail.com>
2021-04-11 00:01:58 +0530

    Added description for queue data structure

--- DataStructures/Queue/queue.js
+++ DataStructures/Queue/queue.js
@@ -1,3 +1,7 @@
+/*Item will be added at one end and can be removed from other end.It can be compared to a line of people purchasing ticket from ticket counter. After purchasing ticket, people will be removed from front and people will get added at back part of queue. Front part is restricted to only removing and back part is restricted to only adding.Process of adding new item to quequ is called enqueuing and process of removing recored from queue is called dequeuing.It follows FIFO principle.
+
+In order to implement a queue we need to restrict shift,push,splice,slice methods provided by native javascript. Only unshift() of Array will be used for adding new record and pop() will be used for removing record.*/
+
 class MyQueue{
     constructor(){
         this.data = []
