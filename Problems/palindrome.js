45de1f0afcb9cac0c26a58ada4010f3df562d691
Jitendra Sabat <jitendrasabat.jitu@gmail.com>
2021-06-23 00:41:31 +0530

    alternative solution for palindrome
    alternative solution for palindrome

--- Problems/palindrome.js
+++ Problems/palindrome.js
@@ -10,9 +10,29 @@ function palindrome(inputString){
     return originalString === finalString;
 }
 
+function palindrome2(inputString){
+    var originalString = inputString.toLowerCase();
+    var stringArray = originalString.split("");
+    /********************************************************* *
+     * every() will run the callback function for every item in
+     * an array and will return true if the underlying condition
+     * will return true for every element else will return false
+    ************************************************************/
+    var isPalindrome = stringArray.every((char,index) => {
+        return char === inputString[inputString.length-index-1];
+    });
+    return isPalindrome;
+}
+
 function checkPalindrome(){
     var str = "abbas";
     console.log(palindrome(str));
 }
 
-checkPalindrome();
+function checkPalindrome2(){
+    var str = "abba";
+    console.log(palindrome2(str));
+}
+
+//checkPalindrome();
+checkPalindrome2();
