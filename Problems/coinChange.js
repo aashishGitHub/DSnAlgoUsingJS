e8479725c6fc17efdb03f9a38973ea66469052de
Aashish Kumar <aashish123pqr@gmail.com>
2022-09-20 08:18:56 +0530

    Update coinChange.js

--- Problems/coinChange.js
+++ Problems/coinChange.js
@@ -1,18 +1,42 @@
-let coinChange = (coints, c, N) => {
-let count = [[]];
+let coinChange = (amount, coins, dp) => {
+  if (amount === 0 || coins.length === 0) {
+    return -1;
+  }
+  let totalNumberOfCoinsRequired = amount;
 
-for(let i = 0; i<=N; i++) {
-    for(let j = 0; j<c; ++j) {
+  for (let i = 0; i < coins.length; i++) {
+    let numberOfCoinsRequiredWHileTakingThisCoin = 0;
 
-        if(i==0 || j== 0) {
+    let coin =  coins[i];
+    
+    if (amount - coin >= 0) {
+      if (dp[amount - coin] === -1) {
+        numberOfCoinsRequiredWHileTakingThisCoin = coinChange(
+          amount - coin,
+          coins,
+          dp
+        );
+      } else {
+        numberOfCoinsRequiredWHileTakingThisCoin = dp[amount - coin];
+      }
 
-            count[i][j] = 1;
-
-        } else {
-            
-        }
+      if (
+        numberOfCoinsRequiredWHileTakingThisCoin >= 0 &&
+        1 + numberOfCoinsRequiredWHileTakingThisCoin <=
+          totalNumberOfCoinsRequired
+      ) {
+        totalNumberOfCoinsRequired =
+          numberOfCoinsRequiredWHileTakingThisCoin + 1;
+          
+      }
     }
-}
+  }
+  dp[amount] = totalNumberOfCoinsRequired;
+  return totalNumberOfCoinsRequired;
+};
 
-
-}
+let amount = 17;
+let coins = [1, 2, 5, 7];
+let dp = new Array(amount).fill(-1);
+let result = coinChange(amount, coins, dp);
+console.log(result);
